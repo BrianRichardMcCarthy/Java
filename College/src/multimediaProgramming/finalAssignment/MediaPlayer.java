2c6ea6f2a710751fa99029dc0d1e09d4890b31c4
brianrichardmccarthy <brianmccarthy1@outlook.ie>
2016-04-29 18:11:28 +0100

    Added method to change selected Image

--- College/src/multimediaProgramming/finalAssignment/MediaPlayer.java
+++ College/src/multimediaProgramming/finalAssignment/MediaPlayer.java
@@ -2,6 +2,8 @@ package multimediaProgramming.finalAssignment;
 
 import java.applet.AudioClip;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.WindowEvent;
@@ -14,12 +16,14 @@ import javax.swing.JLabel;
 
 public class MediaPlayer extends JFrame implements WindowListener, ItemListener {
 	
+	public static Toolkit kit = Toolkit.getDefaultToolkit();
+	private static Dimension screen = kit.getScreenSize();
 	private String 
 							dir = "src\\multimediaProgramming\\finalAssignment\\", 
 							slashFlag = "flags\\", 
 							slashAudio = "anthems\\",
 							currentAnthem = "Denmark.wav",
-							currentFlag = "denmark.gif",
+							currentFlag = "denmark.jpg",
 							selected;
 							;
 	private ImageIcon flag = new ImageIcon(dir + slashFlag + currentFlag);
@@ -33,17 +37,19 @@ public class MediaPlayer extends JFrame implements WindowListener, ItemListener 
 	}
 	
 	public MediaPlayer() {
-		addDropDown();
-		add(labelImage, BorderLayout.EAST);
-		add(country, BorderLayout.NORTH);
+		setSize(screen.width / 2, screen.height / 2);
+		setLocation(screen.width / 4, screen.height / 4);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		addDropDown();
+		add(labelImage, BorderLayout.CENTER);
+		add(country, BorderLayout.NORTH);
 		pack();
 		setVisible(true);
 	}
 	
 	private void addDropDown() {
-		 country.addItem("United States of America");
-		 country.addItem("United Kingdom");
+		 country.addItem("USA");
+		 country.addItem("UK");
 		 country.addItem("Denmark");
 		 country.addItem("Norway");
 		 country.addItem("China");
@@ -89,9 +95,17 @@ public class MediaPlayer extends JFrame implements WindowListener, ItemListener 
 
 	public void itemStateChanged(ItemEvent arg0) {
 		// TODO Auto-generated method stub
+		String[] fileType = {".jpg", ".wav"};
 		selected = (String)country.getSelectedItem();
+		currentAnthem = dir + selected + fileType[1];
+		currentFlag = dir + slashFlag + selected + fileType[0];
+		System.out.println(currentFlag);
+		flag.getImage().flush();
+		flag.setImage(new ImageIcon(currentFlag).getImage());
+		labelImage.imageUpdate(flag.getImage(), 0, 0, 100, 100, 100);
+		labelImage.repaint();
 	}
-
+	
 	public static void main(String[] args) {
 		try {
 			new MediaPlayer();
